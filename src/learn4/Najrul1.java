b5ca0ebaf03128bd0707d6b2743e67d6468cfc9a
AnnanaIrin <msakther786@gmail.com>
2022-04-08 17:30:28 -0400

    first comit

---
+++ src/learn4/Najrul1.java
@@ -0,0 +1,46 @@
+package learn4;
+
+public class Najrul1 {
+
+	
+	public static void main(String args []) {
+		
+		
+		System.out.println("Nazrul is a sergent officer");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/learn4/Najrul2.java
@@ -0,0 +1,40 @@
+package learn4;
+
+public class Najrul2 {
+	
+	
+	
+	public static void main(String args []) {
+		
+		
+		System.out.println("So what does it's take to learn something");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
